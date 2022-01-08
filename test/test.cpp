b775de757f580bd2791367c95bc65a36aa93f0c9
fyt <605325871@qq.com>
2022-01-08 22:25:41 +0800

    2022-1-9

--- test/test.cpp
+++ test/test.cpp
@@ -1,8 +1,37 @@
 # include <iostream>
 using namespace std;
+
+// void swap(int*&a,int *&b)
+// {
+//     int* t;
+//     t=a;
+//     a=b;
+//     b=t;
+// }
+
+// int main()
+// {
+//     int a=10,b=20;
+//     cout<<"a="<<a;
+//     cout<<"b="<<b<<endl;
+
+//     swap(a,b);
+//      cout<<"a="<<a<<endl;
+//     cout<<"b="<<b<<endl;
+
+
+// }   
+
+int & sort()
+{
+    int a=20;
+    return a;
+}
+
 int main()
 {
-    int *p=new int (10);
-
-    cout<<*p<<endl;
+    int &n=sort();
+    int a;
+  cin>>a;
+    cout<<"a="<<a<<endl;
 }
