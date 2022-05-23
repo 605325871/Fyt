6bdbf3dd463754e408ed0078f4adbeeefc123b64
fyt <605325871@qq.com>
2022-05-23 21:17:36 +0800

    mv

---
+++ learnspace/littleproject/vm/main.cpp
@@ -0,0 +1,33 @@
+#include "wm.h"
+int main()
+{
+    wm w;
+    int choice = 0;
+    while (true)
+    {
+        w.show_menu();
+        cin>>choice;
+
+        switch (choice)
+        {
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        default:
+            break;
+        }
+    }
+}
---
+++ learnspace/littleproject/vm/wm.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <iostream>
+#include<stdlib.h>
+using namespace std;
+class wm
+{
+private:
+    /* data */
+public:
+    wm();
+    ~wm();
+    void show_menu();
+};
