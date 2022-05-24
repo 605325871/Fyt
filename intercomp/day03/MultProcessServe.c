727375760291c49a89a72fbec884040f985e2842
fyt <605325871@qq.com>
2022-05-24 23:19:49 +0800

    mult

--- intercomp/day03/MultProcessServe.c
+++ intercomp/day03/MultProcessServe.c
@@ -18,8 +18,7 @@ void perr_exit(const char *s)
 
 void catch_child(int signum)
 {
-    while (waitpid(0, NULL, WNOHANG) > 0)
-        ;
+    while (waitpid(0, NULL, WNOHANG) > 0) ;
     return;
 }
 int main()
@@ -30,37 +29,34 @@ int main()
     char buff[BUFSIZ];
     char str[1024];
     socklen_t client_t;
+
     struct sockaddr_in serv_addr, client;
     memset(&serv_addr, 0, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(SERV_PORT);
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    
+    serv_addr.sin_family = AF_INET; //为serveaddr 结构体设置ipv4协议
+    serv_addr.sin_port = htons(SERV_PORT);//将端口号 从本地小端 转换成大端
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);//在本机中找出空闲的地址
 
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM, 0); // 利用listenfd 来操纵socket结构体的缓冲区
     if (listenfd == -1)
         perr_exit("socket error");
 
-    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1)
+    if (bind(listenfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) == -1) // 将缓冲区与地址结构进行绑定
         perr_exit("bind error");
-    if (listen(listenfd, 128) == -1)
+    if (listen(listenfd, 128) == -1) //设置最大的监听上限
         perr_exit("listen error");
-    printf("received from %s at port %d\n",
-           inet_ntop(AF_INET, &client.sin_addr, str, sizeof(str)),
-           ntohs(client.sin_port));
 
     while (1)
     {
-        connectfd = accept(listenfd, (struct sockaddr *)&client, &client_t);
+        connectfd = accept(listenfd, (struct sockaddr *)&client, &client_t); //阻塞等待处理客户端发来请求
 
-        pid = fork();
+        pid = fork(); //将任务交给子进成管理
         if (pid < 0)
         {
             perr_exit("fork");
         }
         else if (pid == 0)
         {
-            close(listenfd);
+            close(listenfd); //子进程关闭监听描述符
             while (1)
             {
 
@@ -81,7 +77,7 @@ int main()
         }
         else if (pid > 0)
         {
-            struct sigaction act;
+            struct sigaction act;       //父进程等待回收子进程
             act.sa_handler = catch_child;
             sigemptyset(&act.sa_mask);
             act.sa_flags = 0;
@@ -92,7 +88,7 @@ int main()
                 perr_exit("sigaction");
             }
 
-            close(connectfd);
+            close(connectfd); //关闭返回的新描述符
             continue;
         }
     }
