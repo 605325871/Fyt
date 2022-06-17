8fea7af83ded6525ba602ea60924bc3cd44ab455
fyt <605325871@qq.com>
2022-06-17 14:27:38 +0800

    socket

--- intercomp/day01/upserve.cc
+++ intercomp/day01/upserve.cc
@@ -9,134 +9,17 @@
 #include <ctype.h>
 #include <string>
 #include <stdlib.h>
+#include <iostream>
+#include <pthread.h>
+#include "TcpSocket.hh"
 #define SERV_PORT 9527
-class TcpSocket
-{
-public:
-    TcpSocket();
-    TcpSocket(int socket);
-    ~TcpSocket();
-    int connectToHost(std::string ip, unsigned short port);
-    int sendMsg(std::string msg);
-    std::string recvMsg();
-
-private:
-    int readn(char *buf, int size);
-    int writen(const char *msg, int size);
-
-private:
-    int m_fd; // 通信的套接字
-};
-
-TcpSocket::TcpSocket()
-{
-    m_fd = socket(AF_INET, SOCK_STREAM, 0);
-}
-TcpSocket::TcpSocket(int socket)
-{
-    m_fd = socket;
-}
-
-TcpSocket::~TcpSocket()
-{
-    if (m_fd > 0)
-    {
-        close(m_fd);
-    }
-}
-int TcpSocket::connectToHost(std::string ip, unsigned short port)
-{
-    struct sockaddr_in saddr;
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(port);
-
-    inet_pton(AF_INET, ip.data(), &saddr.sin_addr.s_addr);
-    int ret = connect(m_fd, (struct sockaddr *)&saddr, sizeof(saddr));
-    if (ret == -1)
-    {
-        perror("connect error");
-        exit(-1);
-    }
-    return ret;
-}
-int TcpSocket::sendMsg(std::string msg)
-{
-    char *data = new char[msg.size() + 1];
-    uint32_t biglen = htonl(msg.size());
-    memcpy(data, &biglen, 4); //包头四字节存长度+数据长度
-    memcpy(data + 4, msg.data(), msg.size());
-
-    int ret = writen(data, msg.size() + 4);
-    return ret;
-}
-
-int TcpSocket::writen(const char *msg, int size)
-{
-    int left = size;
-    int nwrite = 0;
-    const char *p = msg;
-
-    while (left > 0)
-    {
-        if ((nwrite = write(m_fd, msg, left)) > 0)
-        {
-            p += nwrite;
-            left -= nwrite;
-        }
-        else if (nwrite == -1)
-        {
-            return -1;
-        }
-    }
-    return size;
-}
-
-int TcpSocket::readn(char *buf, int size)
-{
-    int nread = 0;
-    int count = size;
-    char *p = buf;
-    while (count > 0)
-    {
-        if ((nread = read(m_fd, p, count)) > 0)
-        {
-            p += nread;
-            count -= nread;
-        }
-        else if (nread == -1)
-        {
-            return -1;
-        }
-    }
-    return size;
-}
-
-std::string TcpSocket::recvMsg()
-{
-    int len = 0;
-    readn((char *)&len, 4);
-    len = ntohl(len);
-
-    //根据读出的长度从新分配内存
-    char *buf = new char[len + 1];
-    int ret = readn(buf, len);
-    if (ret != len)
-    {
-        return std::string();
-    }
-    buf[len] = '\0';
-    std::string retstr(buf);
-    delete[] buf;
-    return retstr;
-}
-
 class TcpServer
 {
 public:
     TcpServer();
     ~TcpServer();
     int setListen(unsigned short port);
-    TcpSocket *acceptConn(struct sockaddr_in *addr = nullptr);
+    TcpSocket *acceptConn(struct sockaddr_in *addr=nullptr);
 
 private:
     int m_fd; // 监听的套接字
@@ -172,7 +55,7 @@ int TcpServer::setListen(unsigned short port)
     return ret;
 }
 
-TcpSocket *TcpServer::acceptConn(struct sockaddr_in *addr = nullptr)
+TcpSocket *TcpServer::acceptConn(struct sockaddr_in *addr)
 {
     if (addr == nullptr)
     {
@@ -187,11 +70,60 @@ TcpSocket *TcpServer::acceptConn(struct sockaddr_in *addr = nullptr)
     }
     return new TcpSocket(cfd);
 }
+
+struct SockInfo
+{
+    TcpServer *s;
+    TcpSocket *tcp;
+    struct sockaddr_in addr;
+};
+void *working(void *arg)
+{
+    struct SockInfo *pinfo = static_cast<struct SockInfo *>(arg);
+    char ip[INET_ADDRSTRLEN];
+    printf("客户端的ip:%s,端口:%d\n", inet_ntop(AF_INET, &pinfo->addr.sin_addr.s_addr, ip, sizeof(ip))
+     ,ntohs(pinfo->addr.sin_port));
+
+     while (true)
+     {
+         printf("接受数据....\n");
+         std::string msg = pinfo->tcp->recvMsg();
+         if(!msg.empty())
+         {
+            std:: cout<<msg<< std::endl<<std::endl<<std::endl;
+         }
+         else
+         {
+             break;
+         }
+         delete pinfo->tcp;
+         delete pinfo;
+         return nullptr;
+     }
+     
+
+}
+
 int main()
 {
-    TcpSocket tcsocket;
-    int ret = tcsocket.connectToHost("127.0.0.1", 9527);
-    if (ret == -1)
-        return -1;
-    
+    TcpServer s;
+    s.setListen(9527);
+
+    while (true)
+    {
+        SockInfo *info = new SockInfo;
+        TcpSocket *tcp = s.acceptConn(&info->addr);
+        if (tcp == nullptr)
+        {
+            std::cout << "重试...." << std::endl;
+            continue;
+        }
+        pthread_t tid;
+        info->s = &s;
+        info->tcp = tcp;
+
+        pthread_create(&tid, NULL, working, info);
+        pthread_detach(tid);
+    }
+    return 0;
 }
