a7b66b65c70300774e6ef6b9a8ee94c28f5b8a2d
fyt <605325871@qq.com>
2022-06-22 23:24:36 +0800

    tcpserve

---
+++ cjson/UnitOne/leptjson.c
@@ -0,0 +1,24 @@
+#include "leptjson.h"
+#include <assert.h>
+#include <stdlib.h>
+
+#define EXPECT(c, ch)             \
+    do                            \
+    {                             \
+        assert(*c->json == (ch)); \
+        c->json++;                \
+    } while (0);
+
+typedef struct
+{
+    const char *json;
+} lept_context;
+
+static void lept_parse_whitespace(lept_context *c)
+{
+    const char *p = c->json;
+    while (*p == ' ' || *p == '\t' || *p == '\n' || *p == '\r')
+        p++;
+    c->json = p;
+}
+
