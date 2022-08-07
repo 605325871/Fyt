3c2b61478c316c03dbaff657c3a652dcb6002ebd
605325871 <605325871@qq.com>
2022-08-07 22:31:50 +0800

    fix bug

--- learnspace/learning/c11/decltype.cpp
+++ learnspace/learning/c11/decltype.cpp
@@ -14,7 +14,8 @@ int main()
     char h='d';
     //如果没有带参数则推导的时函数的类型
     cout<<typeid(decltype(getmemory)).name()<<endl;
-    //如果带了参数列表，返回的时函数返回值的类型，不会执行函数
+    
+    
     cout<<typeid(decltype(getmemory(0))).name()<<endl;
 
      vector<int> v;
